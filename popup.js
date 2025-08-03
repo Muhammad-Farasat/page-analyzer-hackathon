48548cc39ae48fc578fb774e9a4547576cf37bce
Muhammad-Farasat <farasatkhan@gmai.com>
2025-08-04 00:31:03 +0500

    backend url --> added

--- popup.js
+++ popup.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     result.textContent = "Analyzing...";
 
     try {
-      const res = await fetch("http://localhost:5000/api/chat", {
+      const res = await fetch("https://page-analyzer-be.vercel.app/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
